b678ebb010969e50909dff6776e6611694e394c7
zhang-ning <hunter.dding@gmail.com>
2014-02-07 18:27:21 +0800

    add resume-river

---
+++ resume-river/lang/cn.js
@@ -0,0 +1,49 @@
+define('lang.cn',function(){
+  var cn = {
+    "name" : "邓玉琳",
+    /*"birth-year" : "1983年",
+    "gender" : "女",
+    "location" : "北京",
+    "work-year" : "八年",*/
+    "email" : "dengyulin_112@163.com",
+    "phone" : "18601927019",
+    //"welcome" : "",
+    "introduce" : "你好，我叫邓玉琳，2005年毕业于湖南大学计算机科学与技术专业。从事软件开发工作至今已有8年。前三年，我做的是jsp/java/Oracle的开发；最近这5年，做纯粹的前端开发，专注于javascript, css, html, xslt, svg, canvas等. 在工作中，我是个认真踏实的人，喜欢把技术研究得比较深，把业务逻辑弄清楚及写清楚。我有一些比较有趣的前端的小作品，放在http://www.besideriver.com/river上。下面是我的工作经历。",
+    "works":[{
+      "name":"文思创新 TIBCO中国研发中心 User Experience组",
+      "during":"5 years",
+      "info":"",
+      "projects":[{
+        "name":"Adapters UI",
+        "during":"4个月",
+        "info":"这个项目的前端架构与实现都是我做的，包括css等，基于AngularJS框架。我的架构设计得很直观，没有复杂的配置，几乎找不到if/else，在所见到的地方，就能找到它的控制。将来，只用一个Junior程序员，就可以维护它。我还写了一个纯javascript, svg的带动画的游泳圈图的组件。"
+      },{
+        "name":"TCM",
+        "during":"8 monthes",
+        "info":"我设计了它的文件夹结构，实现了它的导航的功能。导航是有URL-Driven 功能的，即用具体的url可以定位到任意资源，可以点back/forward前进后退。我还开发了它的一些主要模块。"
+      },{
+        "name":"AMX",
+        "during":"2 years",
+        "info":"开发它的一些模块及修bug。这个产品是用TIBCO自己的前端框架GI(General Interface)做的。它是一个很先锋的框架，在很早以前就使用了Ajax，pub/sub，xslt，lazy plugin loading，template language等后来很流行的概念。我是第一个将这个产品的业务含义弄清楚的组员，后来组里做了一个使用该产品的比赛。在我开发的模块中，或我修过bug的地方，再出现bug都很少。"
+      },{
+        "name":"Sportfire",
+        "during":"1 month",
+        "info":"用svg，D3做数据可视化"
+      },{
+        "name":"EA",
+        "during":"2 weeks",
+        "info":"用websocket，long polling做数据推送。后来我自己写了一个对websocket协议的实现，后端用java，前端用js（参考了别人的c#版）。但是没有全部成功。"
+      }
+      ]
+    },{
+      "name":"亿迅(中国)软件有限公司",
+      "during":"1 year",
+      "info":"呼叫中心所软件"
+    },{
+      "name":"拓维信息系统股份有限公司",
+      "during":"2 year",
+      "info":"系统集成，语音短信，动漫等。我所在的组是为湖南移动做12580音乐网站。"
+    }]
+  };
+  return cn;
+});
---
+++ resume-river/lang/en.js
@@ -0,0 +1,48 @@
+define('lang.en',function(){
+  var en = {
+    "name" : "River Deng",
+    /*"birth-year" : "1984",
+    "gender" : "Male",
+    "location" : "Beijing",
+    "work-year" : "6 years",*/
+    "email" : "dengyulin_112@163.com",
+    "phone" : "18601927019",
+    //"welcome" : "",
+    "introduce" : "Hi, you can call me River Deng. I graguated from Hunan University, the Computer Science and Technology, in 2005. Till now, I have been working as a software developer for 8 years. In the first 3 years, I was a JSP/Java/Oracle developer; in this 5 years, I work as a professional front-end developer, focusing on javascript, css, html, xslt, svg, canvas, etc. In the work, I like to study the technology in deep, and write the code with high performance, make clear the project business logic.I have some interesting fron-ent demos on http://besideriver.com/river; the following is my work trace for your reference. ",
+    "works":[{
+      "name":"VanceInfo TIBCO CDC User Experience",
+      "during":"5 years",
+      "info":"",
+      "projects":[{
+        "name":"AdaptersUI",
+        "during":"4 month",
+        "info":"I desinged and implemented the ui architecture and modules basing on AngularJS framework. The architecture I designed has very straitforward constructure, with very few configuration, and almost no if/else. It would can be maintained and understood by a junior develper. These have been able to achieve is because of the advantage of AngularJS framework and the special of the project's business. Besides, I developed a component drawing the occupancy chart with animation in pure javascript and svg."
+      },{
+        "name":"TCM",
+        "during":"8 monthes",
+        "info":"Defined the ui folder structure and implemented the URL-Driven navigation. The URL-Driven means, the special resource can be accessed by using the url with special hash, and by clicking the browser's back/forward buttons, navigates user to the visited resources. I also deveopled some of its main modules."
+      },{
+        "name":"AMX",
+        "during":"2 years",
+        "info":"I devolped some of its modules and fixed bugs for many modules. This product is developed basing on TIBCO's fron-ent framwork, GI(General Interface). It is a pioneer framwork, it used the technologies Ajax, pub/sub, xslt, lazy pugin loading, template language years ago. In this product, I was the first team player who made clear what the product is used for, and this influenced other teammates. In the modules developed by me, or the modules that I ever fixed bugs for, there is fewer bugs."
+      },{
+        "name":"Spotfire",
+        "during":"1 month",
+        "info":"using svg and D3 do the data virtualization"
+      },{
+        "name":"EA",
+        "during":"2 weeks",
+        "info":"websocket, long polling. After the project, I made a websocket principle's implementation using java and javascript, by refering a C# version developed by other. But I didn't get a completely success."
+      }]
+    },{
+      "name":"eSOON China Corporation",
+      "during":"1 year",
+      "info":"Call center software"
+    },{
+      "name":"Talkweb Information System",
+      "during":"2 years",
+      "info":"System integration, ivr, cartoon. My team developed the 12580 music website for China Mobile Hunan branch."
+    }]
+  };
+  return en;
+});
