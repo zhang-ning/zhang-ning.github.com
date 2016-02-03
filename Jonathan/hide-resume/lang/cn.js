602e7ed5d98de011944aa948089af4ada3a765cd
zhang-ning <hunter.dding@gmail.com>
2016-02-04 00:29:54 +0800

    update

--- Jonathan/hide-resume/lang/cn.js
+++ Jonathan/hide-resume/lang/cn.js
@@ -4,14 +4,29 @@ define('lang.cn',function(){
     "birth-year" : "1984年",
     "gender" : "男",
     "location" : "北京",
-    "work-year" : "六年",
+    "work-year" : "八年",
     "phone" : "18513815182",
     "mail" : "hunter.dding@gmail.com",
     "welcome" : "欢迎阅读源代码,\n模块封装的风格受到了CommonJS规范的影响,\n模板引擎的实现受到了AngularJS的影响.\n更多精彩内容经请关注：@猎人丁丁,\n访问我的个人网站 http://besideriver.com",
+    "will" : "熟悉http协议，比较了解xss，csrf安全防御, js,css,正则基本功还可以,此篇简历是用自己曾经做过的一个mvvm框架RiverJS实现的，对express，angular,HttpProxy等工具比较熟悉，可以独立完成全栈工作, 对Mricoservice感兴趣, 经常使用httpProxy实现各个模块间的通信,2015年初攻克了文本的协同编辑技术，获的公司一级技术专利奖励，并服务于公司内网一个产品,稳定可靠,日UV 600+, 协同时可看见彼此选区、光标和头像",
+    "yixiang": "早年从写sql，plsql,shell入行，后来在国内的做舆情检测的小公司从.net和java逐渐成长起来，偶然的机会投入前端并转入前端工作几年，最近期望向全栈或偏后端方向发展，期望有一份稳定可持续发展的工作",
     "introduce" : "对创建有用，易用的应用程序有兴趣，注重应用程序的用户体验的同时注重功能性,6年编程经验，喜欢用vim,tmux在ubuntu上开发,也爱git和node,两年前开始专注前端开发,对javascript动画，svg css3 html5都颇有了解,你可以通过访问我的个人网站了解我在这方面的技术水平.半年前通过使用Google AngularJS和D3开始接触javascript里的双向数据绑定和数据可视化.2个月前，我写了RiverJS,一个灵活小巧的前端框架,提供双向绑定和CMD的模块封装,它类似于AngularJS,但是体积更小.最近开始在npm上发布一些工具.",
     "works":[{
+        "name": "百度FEX团队",
+        "during": "2014.7-至今",
+        "info": "内部工程效率工具和产品研发",
+        "projects": [{
+            "name": "AGroup 和 Markdown协同笔记",
+            "during": "6 month",
+            "info": "类slack产品,核心开发，AngularJS,Mongodb,Redis,Express,Stackeditor,HttpProxy,StackEditor,协同编辑，一级技术专利,ag.baidu.com,agroup.baidu.com "
+        },{
+            "name": "Legend legend.baidu.com h5.baidu.com",
+            "during": "6 month",
+            "info": "类易企秀产品,核心开发,AngularJS,Mongodb,MySQL,Redis,Express,HttpProxy,安全防御，PHP CodeInteger"
+        }]
+      },{
       "name":"文思海辉Tibco部门UX组",
-      "during":"2012.4-2014.5",
+      "during":"2012.4-2014.7",
       "info":"Tibco公司绝大部分产品的UX实现",
       "projects":[{
         "name":"Tibco Comunicate Manager",
--- Jonathan/hide-resume/lang/en.js
+++ Jonathan/hide-resume/lang/en.js
@@ -8,8 +8,23 @@ define('lang.en',function(){
     "phone" : "18513815182",
     "mail" : "hunter.dding@gmail.com",
     "welcome" : "Welcome to read source,\nPart of the module wrapping style is from CommonJS stand,\nand the template engine is effected by AngularJS.\nIf you want to know more,pleate fellow @hunterdding at twitter,\nand welcome to visit my website http://besideriver.com",
+    "will" : "熟悉http协议，比较了解xss，csrf安全防御，对express，angular,HttpProxy等工具比较熟悉，可以独立完成全栈工作, 对Mricoservice感兴趣, 经常使用httpProxy实现各个模块间的通信,2015年初攻克了文本的协同编辑技术，获的公司一级技术专利奖励，并服务于公司内网一个产品,稳定可靠,日UV 600+, 协同时可看见彼此选区、光标和头像",
     "introduce" : "Hello,If you are search a guy who is not only good at UX work, but also have much power at server-side even mobile-end,I'm your guy.Let me take five minutes break to introduce myself.You can call me Jonathan.I have been programming for 6 years,and I like develop on ubuntu via vim and tmux, linux system is fantastic,I learned a lot from it,and I also like the life at Github,read and make a contributes to opensource software.You can find that I'm not ease to stop forward.Two years ago I just fall in love with javascript,and so care about the user experience.at that time I began to focus on the UX thing as a professonal fron-end developer,I joined many projects and more important build a lot compoments with html5,css3,svg under W3C standard.Half year ago I begin to touch two-way data-binding and data visulization in javascript through AngularJS and D3.And Two month ago I wrote RiverJS a small framework but have powerful features like what Angular supply,you can find it at bellow.For node,you also can find my opensource work at NPM by the author name jonathan-zhang.Now you have already know me a littel bit.Hope have opportunity work with you :)",
+    "yixiang": "早年从写sql，plsql入行，后来写过一年多的cshell，后来在国内的做电子政务的小公司从.net和java做起来，偶然的几乎投入前端并转入前端工作几年，最近期望向全栈或偏后端方向发展，期望有一份稳定可持续发展的工作",
     "works":[{
+        "name": "百度FEX团队",
+        "during": "2014.7-至今",
+        "info": "内部工程效率工具和产品研发",
+        "projects": [{
+            "name": "AGroup 和 Markdown协同笔记",
+            "during": "6 month",
+            "info": "类slack产品,核心开发，AngularJS,Mongodb,Redis,Express,Stackeditor,HttpProxy,StackEditor,协同编辑，一级技术专利,ag.baidu.com,agroup.baidu.com "
+        },{
+            "name": "Legend legend.baidu.com h5.baidu.com",
+            "during": "6 month",
+            "info": "类易企秀产品,核心开发,AngularJS,Mongodb,MySQL,Redis,Express,HttpProxy,安全防御，PHP CodeInteger"
+        }]
+    },{
       "name":"UX of Tibco in Pactera",
       "during":"2012.4-2012.5",
       "info":"All the Tibco product's UX work",
